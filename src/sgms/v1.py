359a3684c4af4284748dd0e5705d2e7714f50625
Devi Prasad <dp@web3pleb.org>
2025-06-27 16:04:07 +0530

    Simple Group Messaging System - v1 definition.

---
+++ src/sgms/v1.py
@@ -0,0 +1,29 @@
+"""
+The design and implementation of a simple group messaging system.
+
+Define the following abstractions:
+    Group
+        - create
+        - invite a user to the group
+        - make someone an admin
+        - send a message
+        - fetch messages
+        - delete messages for me
+        - delete messages for everyone
+        - remove user
+        - remove admin privilege
+        - rename group
+        - backup
+        - delete group
+    User
+        - register
+        - accept invitation to join a group
+        - leave a group
+
+    MessageText
+        - create a message text
+
+    MessageEnvelope
+        - send to the group
+
+"""
